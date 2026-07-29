ddba4ddb10bdfa454a46ee22ce13117217c96850
Lavínia <alienzk66@gmail.com>
2026-07-29 17:41:21 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let cores = ["Azul", "Vermelho", "Verde", "Amarelo" ]
+let corSelecionada = cores[1]
+console.log(corSelecionada)
+
+
+console.log(cores[cores.length-1])
+
+let pessoa = {
+    nome: "Cobomi"
+}
+
+console.log(pessoa.nome)
+console.log(`{ Nome : {$pessoa.nome} , idade: pessoa.idade}`)
